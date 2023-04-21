3b594b589589678a90bc1cb53ee5ebe73c013d75
E05 <josephanam@snu.ac.kr>
2023-04-21 11:33:06 +0900

    hello

---
+++ ros/listener.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+
+def callback(msg):
+    rospy.loginfo(rospy.get_caller_id())
+
+def listener():
+#topic name is /turtle1/cmd_vel, topic type is Twist
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('chatter', String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
